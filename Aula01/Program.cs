b33162c93d5a037149bb2e68c9ce8c7931dc0b13
paulocarlosfilho <paulocarlosfilho@gmail.com>
2021-09-14 19:23:17 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Paulo Calros";
+            Console.WriteLine($"Olá {nome}!");
+        }
+    }
+}
